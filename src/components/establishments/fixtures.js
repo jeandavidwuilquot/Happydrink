9fb375b54f4f05706cbbf5e5ff6d5b2b9af03a27
jeandavidwuilquot <david.wuilquot@gmail.com>
2019-08-06 09:26:43 +0200

    1

---
+++ src/components/establishments/fixtures.js
@@ -0,0 +1,32 @@
+export const establishments = [
+    {
+        id          : "0890786GH",
+        name        : "Biberium",
+        description : "Un super bar karaoké"
+    },
+    {
+        id          : "0890786GD",
+        name        : "Brew Dogs",
+        description : "Un super bar à bière"
+    },
+    {
+        id          : "MJLMH0389",
+        name        : "Batonier",
+        description : "Un super bar de fin de soirée"
+    },
+    {
+        id          : "POKIU3567",
+        name        : "Blackout",
+        description : "Superbe établissement, on se rappele rarement sa fin de soirée"
+    },
+    {
+        id          : "KNGE5427",
+        name        : "Le  pamplemouse",
+        description : "Bar à cocktail"   
+    },
+    {
+        id          : "KOLM8754",
+        name        : "Delirium Tremens",
+        description : "Mine assurée!"    
+    }
+]
